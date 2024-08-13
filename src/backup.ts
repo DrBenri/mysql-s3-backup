e84ea85c81db9d412a23bd6a6b5f66e5cb277ae7
hocle <hoc.le@oivan.com>
2024-08-13 11:09:36 +0700

    chore: Update backup script to include deleteLimitedBackups function

--- src/backup.ts
+++ src/backup.ts
@@ -1,5 +1,5 @@
 import { exec } from "child_process";
-import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, ListObjectsV2Command, PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 import { createReadStream, unlink } from "fs";
 import { env } from "./env";
 
@@ -36,15 +36,16 @@ const dumpToFile = async (path: string): Promise<void> => {
   console.log(`Creating dump at ${path}...`);
 
   await new Promise((resolve, reject) => {
-    const host = `--host='${env.BACKUP_DATABASE_HOST}'`;
-    const port = `--port='${env.BACKUP_DATABASE_PORT}'`;
-    const user = `--user='${env.BACKUP_DATABASE_USER}'`;
-    const password = `--password='${env.BACKUP_DATABASE_PASSWORD}'`;
+    const host = `--host=${env.BACKUP_DATABASE_HOST}`;
+    const port = `--port=${env.BACKUP_DATABASE_PORT}`;
+    const user = `--user=${env.BACKUP_DATABASE_USER}`;
+    const password = `--password=${env.BACKUP_DATABASE_PASSWORD}`;
     const databasesToExclude = ['mysql', 'sys', 'performance_schema', 'information_schema', 'innodb'].join('|');
 
     const command = env.BACKUP_DATABASE_NAME
-      ? `mysqldump ${host} ${port} ${user} ${password} ${env.BACKUP_DATABASE_NAME} | gzip > ${path}`
-      : `mysql ${host} ${port} ${user} ${password} -e "show databases;" | grep -Ev "Database|${databasesToExclude}" | xargs mysqldump ${host} ${port} ${user} ${password} --databases | gzip > ${path}`
+      ? `mysqldump ${host} ${port} ${user} -p${env.BACKUP_DATABASE_PASSWORD} ${env.BACKUP_DATABASE_NAME} | gzip > ${path}`
+      : `mysql ${host} ${port} ${user} -p${env.BACKUP_DATABASE_PASSWORD} -e "show databases;" | grep -Ev "Database|${databasesToExclude}" | xargs -I {} mysqldump ${host} ${port} ${user} -p${env.BACKUP_DATABASE_PASSWORD} {} | gzip > ${path}`;
+
 
     if (isDebug()) {
       console.log(`Debug: SQL command: ${command}`);
@@ -94,3 +95,54 @@ export const backup = async (): Promise<void> => {
 
   console.log("Backup successfully created.");
 }
+
+export const deleteLimitedBackups = async (limit: number): Promise<void> => {
+  const bucket = env.AWS_S3_BUCKET;
+  const clientOptions: S3ClientConfig = {
+    region: env.AWS_S3_REGION,
+  };
+
+  if (env.AWS_S3_ENDPOINT) {
+    clientOptions.endpoint = env.AWS_S3_ENDPOINT;
+  }
+
+  const client = new S3Client(clientOptions);
+
+  try {
+    const { Contents } = await client.send(new ListObjectsV2Command({
+      Bucket: bucket,
+      MaxKeys: 1000,
+    }));
+
+    if (!Contents) {
+      console.log('No files found.');
+      return;
+    }
+
+    const files = Contents
+      .map((content) => content.Key)
+      .filter((key): key is string => (key ?? '').startsWith('backup-'))
+      .sort()
+      .reverse();
+
+    const filesToDelete = files.slice(limit);
+
+    if (filesToDelete.length === 0) {
+      console.log('No files to delete.');
+      return;
+    }
+
+    for (const file of filesToDelete) {
+      console.log(`Deleting backup at ${bucket}/${file}...`);
+
+      await client.send(new DeleteObjectCommand({
+        Bucket: bucket,
+        Key: file,
+      }));
+    }
+
+    console.log('Backups successfully deleted.');
+  } catch (error) {
+    console.error('Error during backup deletion:', error);
+  }
+};
