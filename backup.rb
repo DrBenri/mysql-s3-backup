d00db0648a6e8a15a4fb76df4674cb6e06662c59
hocle <hoc.le@oivan.com>
2024-11-05 10:52:38 +0700

    chore: Refactor backup script to improve efficiency and include deleteLimitedBackups function

--- backup.rb
+++ backup.rb
@@ -4,6 +4,7 @@ require 'aws-sdk-s3'
 require 'dotenv/load'  # Add this line to load environment variables from .env
 
 # Configuration
+DB_CONNECTION = ENV['BACKUP_DATABASE_CONNECTION']
 DB_HOST = ENV['BACKUP_DATABASE_HOST']
 DB_PORT = ENV['BACKUP_DATABASE_PORT']
 DB_USERNAME = ENV['BACKUP_DATABASE_USER']
@@ -23,12 +24,15 @@ def backup_and_upload
     backup_file = File.join(BACKUP_DIR, "backup_#{timestamp}.sql")
 
     # Command to backup the database
-    backup_command = "mysqldump -h #{DB_HOST} -P #{DB_PORT} -u #{DB_USERNAME} -p#{DB_PASSWORD} #{DB_NAME} > #{backup_file}"
+    backup_command = if DB_CONNECTION.nil?
+            "mysqldump -h #{DB_HOST} -P #{DB_PORT} -u #{DB_USERNAME} -p#{DB_PASSWORD} #{DB_NAME} > #{backup_file}"
+        else
+            "mysqldump #{DB_CONNECTION} > #{backup_file}"
+        end
 
     if ENV['DEBUG']
         puts "Backup command: #{backup_command}"
     end
-     puts "Backup command: #{backup_command}"
 
     # Execute the backup command
     system(backup_command)
@@ -54,5 +58,26 @@ def backup_and_upload
     end
 end
 
+def cleanup
+    MAX_BACKUPS = 7
+
+    # List all backups
+    backups = Dir.glob("#{BACKUP_DIR}/*.sql.gz").sort
+
+    # Delete old backups
+    if backups.length > MAX_BACKUPS
+    backups_to_delete = backups[0..-(MAX_BACKUPS + 1)]
+    backups_to_delete.each do |backup|
+        File.delete(backup)
+        puts "Deleted old backup: #{backup}"
+    end
+end
+
+end
+
 #Call the backup_and_upload method
+# will backup every 24 hours
 backup_and_upload()
+
+# Call the cleanup method, just keep the last 7 backups
+cleanup()
